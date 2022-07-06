d4883bb1c84f91a758bafaed594c6e2cd57aa16f
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-06 15:07:06 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import "./App.scss";
+import Header from "./components/Header/Header";
 import Item from "./components/Item/Item";
 import List from "./components/List/List";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route index element={<List />} />
         <Route path="item/*" element={<Item />} />
