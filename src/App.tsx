15349d3f58509e4675e6171f0e4e0617aaa4f338
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-06 13:48:37 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.scss";
+import Item from "./components/Item/Item";
+import List from "./components/List/List";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route index element={<List />} />
+        <Route path="item/*" element={<Item />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
