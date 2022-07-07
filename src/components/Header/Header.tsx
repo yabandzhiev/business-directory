51947bc9d9ec291c1c38bcaa262981b70c39d508
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-07 03:01:01 +0300

    add logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
+import logo from "../../assets/devOceanLogo.png";
+
 import "./Headers.scss";
 
 const Header = () => {
-  return <div className="header">LOGO</div>;
+  return (
+    <div className="header">
+      <img className="logo" src={logo} alt="logo" />
+    </div>
+  );
 };
 
 export default Header;
