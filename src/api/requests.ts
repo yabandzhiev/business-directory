eac35b407a5681244382cb41d423986aa5016232
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-07 03:00:31 +0300

    add type

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
+import { businessProps } from "../types/businessTypes";
 
 export const getData = async () => {
   const result = await axios.get(
     "https://feinterviewtask.azurewebsites.net/b/6231abada703bb67492d2b8f"
   );
-  return result;
+  const data: businessProps[] = result.data;
+  return data;
 };
