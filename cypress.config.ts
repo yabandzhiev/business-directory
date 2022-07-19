c79166217eee97d65b55620e0f7b0df074560459
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-19 13:57:44 +0300

    configure

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
   },
 });
